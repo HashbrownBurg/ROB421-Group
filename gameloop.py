a26599c87bf4478772d4443936930aad4d70a2ec
HashbrownBurg <hashbarl@oregonstate.edu>
2025-05-14 22:56:46 -0700

    python version

---
+++ gameloop.py
@@ -0,0 +1,65 @@
+from espeak_python_easy_interface import text_to_speech
+from trivia_stt import recognize_speech_from_mic
+import speech_recognition as sr
+# import pyttsx3
+
+class Question:
+    def __init__(self, id, category, question, answer):
+        self.id = id
+        self.category = category
+        self.question = question
+        self.answer = answer
+
+class Game:
+    def __init__(self, num_questions):
+        self.num_questions = num_questions
+        self.current_question_index = 0
+        self.questions = self.load_questions()
+        self.current_question = None
+
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+        # engine = pyttsx3.init()
+
+
+    def load_questions(self):
+        return [
+            Question(1, 'trivia', 'What is my favorite color?', 'blue'),
+            Question(2, 'trivia', 'What planet is known as the Red Planet?', 'mars'),
+            Question(3, 'trivia', 'What is the capital of France?', 'paris'),
+            Question(4, 'trivia', 'What do bees make?', 'honey'),
+            Question(5, 'trivia', 'What gas do plants breathe in?', 'carbon dioxide')
+        ]
+
+    def ask_question(self, question):
+        # text_to_speech(question.question)
+        print(f"Question: {question.question}")
+
+    def get_guess(self):
+        guess =  recognize_speech_from_mic(self.recognizer, self.microphone)
+        print(guess)
+        return guess
+
+    def check_answer(self, question, guess):
+        correct = question.answer.strip().lower() == guess['transcription']
+        correct = "The Answer is Correct" if correct else "The Answer is Incorrect"
+        # text_to_speech(question.question)
+        return correct
+
+    def gameloop(self):
+        while self.current_question_index < self.num_questions:
+            self.current_question = self.questions[self.current_question_index]
+            self.ask_question(self.current_question)
+            guess = self.get_guess()
+            correct = self.check_answer(self.current_question, guess)
+            if correct:
+                self.current_question_index += 1
+
+        print("Game Over.")
+
+def main():
+    game = Game(num_questions=5)
+    game.gameloop()
+
+if __name__ == '__main__':
+    main()
---
+++ trivia_question_finder.py
@@ -0,0 +1,47 @@
+
+import inquirer
+import pandas
+import random
+
+
+def csvAquirer(filePath="Trivia_Questions.csv"):
+    fileInfo = pandas.read_csv(filePath)
+
+    return fileInfo
+
+def pick_Question(questions):
+
+    Q = questions["Questions"]
+    correct = questions["CorrectAnswer"]
+    wrong = questions["BadAnswer"]
+    opt1 = questions["Answer1"]
+    opt2 = questions["Answer2"]
+    opt3 = questions["Answer3"]
+    opt4 = questions["Answer4"]
+    opt5 = questions["Answer5"]
+    negResponse = questions["BadResponse"]
+
+    numPrompts = list(range(0,len(Q)))
+    while len(Q) != 0:
+        rand = random.randint(0,(len(questions["Questions"])-1))
+        index = numPrompts.pop(rand)
+        options = [opt1.pop(index), opt2.pop(index), opt3.pop(index), opt4.pop(index), opt5.pop(index)]
+        random.shuffle(options)
+        answer = inquirer.list_input(message=Q.pop(index), choices=options)
+        
+        badAnswer = wrong.pop(index)
+        goodAnswer = correct.pop(index)
+        rob_response = negResponse.pop(index)
+
+        if answer == goodAnswer:
+            print("Wonderful! That is correct!")
+        elif answer == badAnswer:
+            print(rob_response)
+        else:
+            print("Unfortunately, that is incorrect.")
+
+
+if __name__ == "__main__":
+    fileInfo = csvAquirer()
+
+    pick_Question(fileInfo)
